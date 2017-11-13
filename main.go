f039a1f2743259f3313a244dcbc95877f70bb884
Kenneth Shaw <kenshaw@gmail.com>
2017-11-13 12:44:39 +0700

    Adding redirect + changing go get logic

--- main.go
+++ main.go
@@ -24,14 +24,27 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		// handle the go-get stuff
 		if req.URL.Query().Get("go-get") == "1" {
 			repo := strings.TrimPrefix(req.URL.Path, "/")
 			if i := strings.Index(repo, "/"); i != -1 {
 				repo = repo[:i]
 			}
+
+			// determine the repository
 			repo = strings.TrimSuffix(repo, ".git")
 			url := "https://github.com/go-jakarta/" + repo + ".git"
+			if strings.ToLower(repo) == "gojakarta" {
+				url = "https://github.com/go-jakarta/slides.git"
+			}
+
 			fmt.Fprintf(res, gitHTML, `gophers.id/`+repo+` git `+url, url, url)
+			return
+		}
+
+		// do a redirect ...
+		if strings.ToLower(strings.TrimPrefix(req.URL.Path, "/")) == "gojakarta" {
+			http.Redirect(res, req, "https://meetup.com/GoJakarta", http.StatusMovedPermanently)
 			return
 		}
 
